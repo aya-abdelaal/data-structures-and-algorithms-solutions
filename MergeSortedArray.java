d391685c9067166bf147c0a9b02a0ee53011b79f
Aya <67323302+aya-abdelaal@users.noreply.github.com>
2023-06-28 17:11:37 +0300

    Update MergeSortedArray.java
    working from the end makes it faster, eliminates the need for shifting

--- MergeSortedArray.java
+++ MergeSortedArray.java
@@ -1,28 +1,20 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = 0, j = 0, pos = 0;
-        while( i < m && j < n){
-            if(nums1[pos] < nums2[j]){
-                i++;
-                pos++;
-            }
-            else{
-               for(int k = pos + (m - i); k > pos; k--){
-                    nums1[k] = nums1[k -1];
-               }
-               nums1[pos] = nums2[j];
-               j++;
-               pos++;
+       int i = m-1, j = n-1, pos = nums1.length - 1;
 
-            }
-        }
+       while(i >= 0 && j >= 0){
+           if(nums1[i] > nums2[j]){
+               nums1[pos--] = nums1[i--];
+           }
+           else{
+               nums1[pos--] = nums2[j--];
+           }
+       }
 
-        if(j != n){
-            while(j < n){
-                nums1[pos] = nums2[j];
-                j++;
-                pos++;
-            }
-        }
+       if(j >= 0){
+           while(j >=0){
+               nums1[pos--] = nums2[j--];
+           }
+       }
     }
 }
