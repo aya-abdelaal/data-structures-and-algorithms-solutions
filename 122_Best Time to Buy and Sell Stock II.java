6362c306fb6fd25058595389b165f2c009893866
Aya <67323302+aya-abdelaal@users.noreply.github.com>
2023-07-01 12:41:33 +0300

    Update 122_Best Time to Buy and Sell Stock II.java

--- 122_Best Time to Buy and Sell Stock II.java
+++ 122_Best Time to Buy and Sell Stock II.java
@@ -4,26 +4,16 @@ class Solution {
         if(prices.length == 1)
             return 0;
 
-        int left = 0;
-        int right = 1;
+        int i = 1;
         int profit = 0;
-        int maxProf = 0;
 
-        while(right < prices.length){
-            if(prices[right] < prices[right - 1]){
-                //buy stock 
-                profit += maxProf;
-                maxProf = 0;
-                left = right;
-            } else {
-                int tempProf = prices[right] - prices[left];
-                maxProf = (maxProf > tempProf)? maxProf : tempProf;
+        while(i < prices.length){
+            if(prices[i - 1] < prices[i]){
+                //buy stock yesterday and sell today
+                profit += prices[i] - prices[i - 1];
             }
-            right++;
+            i++;
         }
-
-        if(prices[prices.length - 1] >= prices[prices.length - 2])
-            profit += maxProf;
 
         return profit;
     }
